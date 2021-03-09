3e106474f3672b57718548af48d109cac0379c53
Victor Chetvertukhin <shetvertuhin.viktor@icloud.com>
2021-03-09 16:31:58 +0300

    Insert to empty tree left node 1 green

--- AVLTree/AVLTree/AVLTree.swift
+++ AVLTree/AVLTree/AVLTree.swift
@@ -2,4 +2,8 @@ public struct AVLTree {
     
     let key: Int
     
+    func insert(_ node: AVLTree) {
+        
+    }
+    
 }
--- AVLTree/AVLTreeTests/AVLTreeTests.swift
+++ AVLTree/AVLTreeTests/AVLTreeTests.swift
@@ -7,5 +7,11 @@ class AVLTreeTests: XCTestCase {
         let sut = AVLTree(key: 3)
         XCTAssertEqual(sut.key, 3)
     }
+    
+    func testInsertLeftToEmpty() {
+        let sut = AVLTree(key: 3)
+        let left = AVLTree(key: 2)
+        sut.insert(left)
+    }
 
 }
